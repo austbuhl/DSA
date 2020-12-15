9bed2a57c3d64525fc9936f478e2e73348afb32f
AB <austinbuhler@gmail.com>
2020-12-14 20:44:35 -0500

    Graph Traversal

--- Data Structures/Graph.js
+++ Data Structures/Graph.js
@@ -29,4 +29,81 @@ class Graph {
 
     delete this.adjacencyList[vertex]
   }
+
+  DFSRecursive(start) {
+    const result = []
+    const visited = {}
+
+    const DFS = (vertex) => {
+      if (!vertex) return null
+      result.push(vertex)
+      visited[vertex] = true
+      this.adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return DFS(neighbor)
+        }
+      })
+    }
+
+    DFS(start)
+
+    return result
+  }
+
+  DFSIterative(start) {
+    const stack = [start]
+    const result = []
+    const visited = {}
+
+    visited[start] = true
+    let currentVertex
+    while (stack.length) {
+      currentVertex = stack.pop()
+      result.push(currentVertex)
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true
+          stack.push(neighbor)
+        }
+      })
+    }
+
+    return result
+  }
+
+  BFS(start) {
+    const queue = [start]
+    const result = []
+    const visited = {}
+
+    visited[start] = true
+    let currentVertex
+    while (queue.length) {
+      currentVertex = queue.shift()
+      result.push(currentVertex)
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true
+          queue.push(neighbor)
+        }
+      })
+    }
+    return result
+  }
 }
+
+let g = new Graph()
+g.addVertex('A')
+g.addVertex('B')
+g.addVertex('C')
+g.addVertex('D')
+g.addVertex('E')
+g.addVertex('F')
+
+g.addEdge('A', 'B')
+g.addEdge('A', 'C')
+g.addEdge('B', 'D')
+g.addEdge('C', 'E')
+g.addEdge('D', 'E')
+g.addEdge('D', 'F')
+g.addEdge('E', 'F')
